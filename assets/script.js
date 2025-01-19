a456074eaf0553a520247f89e24e40773e3fbb1c
boulyou tijari <tijarim7@gmail.com>
2025-01-19 14:47:07 +0100

    modifier le responsive

--- assets/script.js
+++ assets/script.js
@@ -18,54 +18,6 @@ const slides = [
 ];
 
 
-// // Sélection des éléments
-// const bannerImage = document.querySelector('.banner-img');
-// const bannerText = document.querySelector('.text');
-// const dots = document.querySelectorAll('.dot');
-// const arrowLeft = document.querySelector('.arrow.arrow_left');
-// const arrowRight = document.querySelector('.arrow.arrow_right');
-
-// let index = 0;
-
-// // Initialisation
-// function initSlide() {
-//     bannerImage.src = `./assets/images/slideshow/${slides[index].image}`;
-//     bannerText.innerHTML = slides[index].tagLine;
-//     dots.forEach(dot => dot.classList.remove('dot_selected'));
-//     dots[index].classList.add('dot_selected');
-// }
-// initSlide();
-
-// // Changement de slide
-// function changeSlide(direction) {
-//     index = (index + direction + slides.length) % slides.length; // Gestion du débordement
-//     initSlide();
-// }
-
-// // Événements sur les flèches
-// arrowLeft.addEventListener('click', () => changeSlide(-1));
-// arrowRight.addEventListener('click', () => changeSlide(1));
-
-
-
-// //Événements sur les dots(en-plus)
-// dots.forEach((dot, dotIndex) => {
-//     dot.addEventListener('click', () => {
-//         if (index !== dotIndex) { // Vérifie si le dot cliqué est différent du slide actuel
-//             index = dotIndex; // Change l'index pour celui correspondant au dot cliqué
-//             initSlide(); // Met à jour le slide
-//         }
-//     });
-// });
-
-
-
-
-
-
-
-
-
 // Sélection des éléments
 const bannerImage = document.querySelector('.banner-img');
 const bannerText = document.querySelector('.text');
@@ -74,8 +26,6 @@ const arrowLeft = document.querySelector('.arrow.arrow_left');
 const arrowRight = document.querySelector('.arrow.arrow_right');
 
 let index = 0;
-let startX = 0; // Position initiale du doigt
-let endX = 0;   // Position finale du doigt
 
 // Initialisation
 function initSlide() {
@@ -96,7 +46,9 @@ function changeSlide(direction) {
 arrowLeft.addEventListener('click', () => changeSlide(-1));
 arrowRight.addEventListener('click', () => changeSlide(1));
 
-// Événements sur les dots (en plus)
+
+
+//Événements sur les dots(en-plus)
 dots.forEach((dot, dotIndex) => {
     dot.addEventListener('click', () => {
         if (index !== dotIndex) { // Vérifie si le dot cliqué est différent du slide actuel
@@ -106,27 +58,10 @@ dots.forEach((dot, dotIndex) => {
     });
 });
 
-// Gestion du glissement tactile
-bannerImage.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].clientX; // Position du doigt au début
-});
 
-bannerImage.addEventListener('touchmove', (e) => {
-    endX = e.touches[0].clientX; // Mise à jour de la position du doigt en mouvement
-});
 
-bannerImage.addEventListener('touchend', () => {
-    if (startX - endX > 50) {
-        // Glissement vers la gauche
-        changeSlide(1);
-    } else if (endX - startX > 50) {
-        // Glissement vers la droite
-        changeSlide(-1);
-    }
-    // Réinitialiser les positions
-    startX = 0;
-    endX = 0;
-});
+
+
 
 
 
